555867b591c57c5dec9f0d0936a32beb8343bb06
josezanle <jose.rios.lm@gmail.com>
2021-05-16 16:27:30 -0300

    adding Head component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
+import Head from "next/head";
 import { NextSeo } from "next-seo";
-import "aos/dist/aos.css";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
@@ -36,6 +37,9 @@ function MyApp({ Component, pageProps }) {
           site_name: "BigBox Challenge",
         }}
       />
+      <Head>
+        <link rel="icon" href="/icons/gem.svg" />
+      </Head>
     </>
   );
 }
