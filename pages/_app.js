95398ce02b1228358edc2bfd5a0d6c38a1bb67f3
josezanle <jose.rios.lm@gmail.com>
2021-05-16 16:13:48 -0300

    adding SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import { NextSeo } from "next-seo";
+import "aos/dist/aos.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <NextSeo
+        title="BigBox Challenge"
+        description="This example uses more of the available config options."
+        canonical="https://bigbox-test.vercel.app/"
+        openGraph={{
+          url: "https://bigbox-test.vercel.app/",
+          title: "FrontEnd Challenge",
+          description: "Welcome to the Bigbox's Frontend Challenge 2021",
+          images: [
+            {
+              url:
+                "https://firebasestorage.googleapis.com/v0/b/zanle-group.appspot.com/o/banner-challenge.jpg?alt=media&token=49878795-6d6c-465a-ba63-2d05dad5bc78",
+              width: 551,
+              height: 344,
+              alt: "Og Image Alt",
+            },
+            {
+              url: "/icons/gem.svg",
+              width: 96,
+              height: 96,
+              alt: "Og Image Alt Second",
+            },
+            {
+              url:
+                "https://firebasestorage.googleapis.com/v0/b/zanle-group.appspot.com/o/banner-challenge.jpg?alt=media&token=49878795-6d6c-465a-ba63-2d05dad5bc78",
+            },
+          ],
+          site_name: "BigBox Challenge",
+        }}
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
